a4163d7c6ac4cbf240af2159469e6c0257b28dce
Yesenia <yeseniasescarcega@gmail.com>
2017-08-10 06:27:15 -0500

    Se prueba función para crear dinámicamente elementos.

--- assets/js/agendaDiaria.js
+++ assets/js/agendaDiaria.js
@@ -37,6 +37,7 @@ function validarEdo() {
 	var edo = agregarEv.textContent;
 	if (edo == 'add') {
 		agendarEv();
+		swal("¡Super!", "El evento ha sido agregado a tu agenda", "success")
 	}
 	else{
 		mostrarAlert();
@@ -50,8 +51,82 @@ function agendarEv() {
 
 var agregarEv = document.getElementById('btn-agregar');
 agregarEv.addEventListener('click', validarEdo);
+// console.log(agregarEv);
+// agregarEv.map(function (item) {
+// 	item.addEventListener('click', validarEdo);
+// });
 
+//
+
+//Función API
+fetch("../api/eventos.json").then(function(respuesta){
+          return respuesta.json();
+      }).then(function(datos){
+        //   console.log(datos.Sheet1);
+          var validation_messages = datos;
+          var arregloDatos = [];
+          for (var key in validation_messages) {
+              if (!validation_messages.hasOwnProperty(key)) continue;
+              var obj = validation_messages[key];
+              for (var prop in obj) {
+                  arregloDatos.push(obj[prop]);
+              }
+          }
+        //   console.log(arregloDatos);
+          console.log(arregloDatos[0].nombre);
+
+      });
+// Función para mostrar tarjetas de los eventos
+var cont = 0;
+var plantillaModal = '<div class="modal-content">'+
+	'<div class="row fondo--azulMedio">'+
+		'<div class="col s1">'+
+			'<i class="material-icons modal-close">close</i>'+
+		'</div>'+
+		'<h4 class="center p-3 letra--blanca  col s10">Titulo Evento</h4>'+
+	'</div>'+
+	  '<a class="right btn-floating btn-large waves-effect waves-light fondo-Az-osc mt-015 izq-3"><i class="material-icons" id="btn-agregar">add</i></a>'+
+	  '<h5>Día 1</h5>'+
+	  '<h6> 0:00 a 0:00</h6>'+
+	  '<h6>Lugar: Salón 405</h6>'+
+	  '<p class="justificar">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae culpa, repellendus aspernatur aliquid reprehenderit perspiciatis libero enim ullam harum amet accusamus deserunt omnis id rerum architecto natus iste ratione illo.</p>'+
+	'</div>';
+
+var plantillaTarjeta = '<section class="card-panel grey lighten-4">'+
+			'<div class="row">'+
+				'<div class="col s8">'+
+					'<h5 class="tituloEv"> __titulo__</h5>'+
+					'<p> __ponente__</p>'+
+					'<div class="container">'+
+						'<div class="row">'+
+							'<span class="center tag col s5 chip"> __sala__ </span>'+
+							'<a href="__id__" class="center offset-s1 col s5"> ver más</a>'+
+						'</div>'+
+					'</div>'+
+				'</div>'+
+				'<div class="center col s4 grey lighten-2">'+
+					'<p class="center chip fuente-Az-med ocultar"> Agendado</p>'+
+					'<h4 class="center pb-9"> 00:00<br> a <br>00:00 </h4>'+
+				'</div>'+
+			'</div>'+
+		'</section>';
+
+function pintarTarjetas() {
+	var tarjetaEvn = "";
+	var contenedorTarjetas = document.getElementById('eventos-Diarios');
+
+}
+
+function pintarModal() {
+	var tarjetaModal = "";
+	var contenedorModal = document.getElementById('modal-Evento');
+}
+
+// Función principal
+function cargarFns() {
+	$('.modal').modal();
+	// pintarTarjetas();
+	// pintarModal();
+}
 // Función al cargar página
-$(document).ready(function(){
-    $('.modal').modal();
-});
+$(document).ready(cargarFns);
